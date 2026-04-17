38a0cb0ed2d1e15326332c7fd9ff76ca192a7a27
RaulToainga34 <rabetomo542@gmail.com>
2026-04-17 17:18:53 -0500

    PARTE 2: Se creo la funcion graficarComida e iniciarJuego, se modifico el evento onload se definieron variables y constantes ademas se modifico la funcion iniciarJuego para que llame a las funciones graficarGato y graficarComida

--- cazando.js
+++ cazando.js
@@ -1,8 +1,30 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+const alto_Gato = 50;
+const ancho_Gato = 50;
+let gatoX = 250;
+let gatoY = 250;
+
+const alto_Comida = 40;
+const ancho_Comida = 40;
+let comidaX = 450;
+let comidaY = 450;
+
 function graficarGato(){
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(250,435,50,50);
+    ctx.fillRect(250,250,50,50);
 
 }
+
+function graficarComida(){
+    ctx.fillStyle = "#000080";
+    ctx.fillRect(450,450,40,40);
+
+}
+
+function iniciarJuego(){
+    graficarComida();
+    graficarGato();
+}
+
